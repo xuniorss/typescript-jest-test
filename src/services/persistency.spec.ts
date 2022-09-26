4dc38619fc1ecfab1c3b7a1272375c9b637b37fa
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-26 19:04:47 -0300

    initial commit

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,9 @@
+it('descricao do test(IT)', () => {
+    const number = 1
+    expect(number).toBe(1)
+})
+
+test('descricao do teste(TESTE)', () => {
+    const nome = 'Junior'
+    expect(nome).toBe('Junior')
+})
