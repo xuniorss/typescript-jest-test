e43907469a7138278d1933bec219a26f42cc41cf
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-26 20:04:24 -0300

    messaging.spec

---
+++ src/services/messaging.spec.ts
@@ -0,0 +1,26 @@
+import { Messaging } from "./messaging";
+
+const createSut = () => { return new Messaging() }
+
+describe('Messaging', () => {
+    afterEach(() => jest.clearAllMocks())
+
+    it('should return undefined', () => {
+        const sut = createSut()
+        expect(sut.sendMessage('teste')).toBeUndefined()
+    })
+
+    it('should call console.log once', () => {
+        const sut = createSut()
+        const consoleSpy = jest.spyOn(console, 'log')
+        sut.sendMessage('teste')
+        expect(consoleSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call console.log with "Mensagem enviada:" and msg', () => {
+        const sut = createSut()
+        const consoleSpy = jest.spyOn(console, 'log')
+        sut.sendMessage('teste')
+        expect(consoleSpy).toHaveBeenCalledWith('Mensagem enviada:', 'teste')
+    })
+})
