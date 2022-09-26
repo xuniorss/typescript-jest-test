2f4ccf63f83d01772106ca68fab3d3682bfccd29
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-26 20:39:24 -0300

    discount.spec

---
+++ src/classes/discount.spec.ts
@@ -0,0 +1,24 @@
+import { Discount, FiftyPercentDiscount, TenPercentDiscount, NoDiscount } from "./discount";
+
+const createSut = (className: new () => Discount): Discount => {
+    return new className()
+}
+
+describe('Product', () => {
+    afterEach(() => jest.clearAllMocks())
+
+    it('should have no discount', () => {
+        const sut = createSut(NoDiscount)
+        expect(sut.calculate(10.99)).toBeCloseTo(10.99)
+    })
+
+    it('should apply 50% discount on price', () => {
+        const sut = createSut(FiftyPercentDiscount)
+        expect(sut.calculate(150.5)).toBeCloseTo(75.25)
+    })
+
+    it('should apply 10% discount on price', () => {
+        const sut = createSut(TenPercentDiscount)
+        expect(sut.calculate(150.5)).toBeCloseTo(135.45)
+    })
+})
