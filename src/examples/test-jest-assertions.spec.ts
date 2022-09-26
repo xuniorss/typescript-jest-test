1d7236d6455d99330f9971304c5ce22abc1fb957
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-26 19:35:44 -0300

    assercoes com jest

---
+++ src/examples/test-jest-assertions.spec.ts
@@ -0,0 +1,34 @@
+describe('Primitive values', () => {
+    it('should test jest assertions', () => {
+        const number = 10
+
+        expect(number).toBe(10)
+        expect(number).toEqual(10)
+
+        expect(number).not.toBeFalsy()
+        expect(number).toBeTruthy()
+
+        expect(number).toBeGreaterThan(9)
+        expect(number).toBeGreaterThanOrEqual(10)
+        expect(number).toBeLessThan(11)
+        expect(number).toBeLessThanOrEqual(10)
+
+        expect(number).toBeCloseTo(10.001)
+        expect(number).not.toBeNull()
+
+        expect(number).toHaveProperty('toString')
+    })
+})
+
+describe('Objects', () => {
+    it('should test jest assertions with objects', () => {
+        const person = { name: 'Junior', age: 24 }
+        const anotherPerson = { ...person }
+
+        expect(person).toEqual(anotherPerson)
+        expect(person).toHaveProperty('age')
+        expect(person).toHaveProperty('age', 24)
+
+        expect(person.name).toBe('Junior')
+    })
+})
