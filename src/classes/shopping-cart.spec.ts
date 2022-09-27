ea51d022e8f2e4ec9367d548b82864c8fc502c0e
Junior Fortunato <fortunatojunior2020@gmail.com>
2022-09-26 21:45:05 -0300

    shopping-cart.spec

---
+++ src/classes/shopping-cart.spec.ts
@@ -0,0 +1,66 @@
+import { ShoppingCart } from "./shopping-cart"
+import { Discount } from "./discount"
+import { CartItem } from "./interfaces/cart-item"
+
+const createSut = () => {    
+    const discountMock = createDiscountMock()
+    const sut = new ShoppingCart(discountMock)
+    return { sut, discountMock }
+}
+
+const createDiscountMock = () => {
+    class DiscountMock extends Discount {}
+    return new DiscountMock()
+}
+
+const createCartItem = (name: string, price: number) => {
+    class CartItemMock implements CartItem {
+        constructor(public name: string, public price: number) {}
+    }
+    return new CartItemMock(name, price)
+}
+
+const createSuitWithProducts = () => {
+    const { sut, discountMock } = createSut()
+    const cartItem1 = createCartItem('Camisa', 40)
+    const cartItem2 = createCartItem('Caneta', 1)
+    sut.addItem(cartItem1)
+    sut.addItem(cartItem2)
+
+    return { sut, discountMock }
+}
+
+describe('ShoppingCart', () => {
+    it('should be an empty cart when no product is added', () => {
+        const { sut } = createSut()
+        expect(sut.isEmpty()).toBe(true)
+    })
+
+    it('should have 2 cart items', () => {
+        const { sut } = createSuitWithProducts()
+        expect(sut.items.length).toBe(2)
+    })
+
+    it('should test total and totalWithDiscount', () => {
+        const { sut } = createSuitWithProducts()
+        expect(sut.total()).toBe(41)
+        expect(sut.totalWithDicount()).toBe(41)
+    })
+
+    it('should add products and clear cart', () => {
+        const { sut } = createSuitWithProducts()
+        expect(sut.items.length).toBe(2)
+        sut.clear()
+        expect(sut.items.length).toBe(0)
+        expect(sut.isEmpty()).toBe(true)
+    })
+
+    it('should remove products and clear cart', () => {
+        const { sut } = createSuitWithProducts()
+        expect(sut.items.length).toBe(2)
+        sut.removeItem(1)
+        expect(sut.items.length).toBe(1)
+        sut.removeItem(0)
+        expect(sut.isEmpty()).toBe(true)
+    })
+})
